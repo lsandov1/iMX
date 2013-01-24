4da0df1c4f0354f55910eb932b5ecca5b96d2e74
Leonardo Sandoval <leonardo.sandoval@freescale.com>
2013-01-24 15:38:58 -0600

    AN4509 script & u-boot update

--- app-notes/AN4509/scripts/power/non-multimedia.sh
+++ app-notes/AN4509/scripts/power/non-multimedia.sh
@@ -1,11 +1,20 @@
 #!/bin/sh
 
 # create the mounting points
-mkdir emmc
-mkdir usb
+if [ ! -d "emmc" ]; then
+
+        mkdir emmc
+
+fi
+
+if [ ! -d "usb" ]; then
+        mkdir usb
+fi
+
+emmc_dir = $(cat /proc/partitions | grep mmcblk.p1 | awk '{print $4}')
 
 # mount the USB and EMMC
-mount /dev/mmcblk0p1 emmc  #mount the emmc
+mount /dev/$emmc_dir emmc  #mount the emmc
 mount /dev/sda1 usb        #mount the usb
 
 # Do the copy, blocks size are 1M, and 1000 of them are copied.

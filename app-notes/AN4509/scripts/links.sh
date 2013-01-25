915f6173ce92ff2f5a3e80abb42cc0480bb199f5
Leonardo Sandoval <leonardo.sandoval@freescale.com>
2013-01-25 17:31:00 -0600

    AN4509: removing links.sh

--- app-notes/AN4509/scripts/links.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-boot_url='https://community.freescale.com/servlet/JiveServlet/download/93887-3-255427/u-boot.bin.zip'
-kernel_1_url='https://community.freescale.com/servlet/JiveServlet/download/93887-3-255426/uImage.zip'
-kernel_2_url='https://community.freescale.com/servlet/JiveServlet/download/93887-3-255425/uImageMaxPower.zip'

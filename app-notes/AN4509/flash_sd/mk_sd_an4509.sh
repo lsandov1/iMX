6e912b84fb09950e425ca60877229d16315ee734
Leonardo Sandoval <leonardo.sandoval@freescale.com>
2013-01-28 11:44:41 -0600

    an4509: print the current step being executed on console

--- app-notes/AN4509/flash_sd/mk_sd_an4509.sh
+++ app-notes/AN4509/flash_sd/mk_sd_an4509.sh
@@ -6,29 +6,34 @@ dev=$1
 tmp_folder=`mktemp -d`
 cd $tmp_folder
 
-# Download scripts
+echo "Downloading scripts"
 curl    --silent https://raw.github.com/lsandoval/iMX/master/flash/mk_mx_sd > mk_mx_sd
-curl    --silent https://raw.github.com/lsandoval/iMX/master/app-notes/AN4509/scripts/mk_image > mk_image
+curl    --silent https://raw.github.com/lsandoval/iMX/master/app-notes/AN4509/power_scripts/mk_image > mk_image
 chmod +x ./mk_mx_sd ./mk_image
            
-# Download the binaries located on 
+echo "Download binaries"
 wget    --quiet \
         http://vfae-server:8000/AN/AN4509/rootfs.tar.bz2 \
         http://vfae-server:8000/AN/AN4509/uImage \
         http://vfae-server:8000/AN/AN4509/uImageMaxPower
         
-# Untar the filesystem
+echo "Unpack the root filesystem"
 tmp_rootfs_folder=`mktemp -d`
 sudo tar xjf rootfs.tar.bz2 -C $tmp_rootfs_folder
                 
-# Flash the SD
+echo "Flash the SD"
 sudo ./mk_image $dev \
                 u-boot.bin \
                 uImage \
                 uImageMaxPower \
                 $tmp_rootfs_folder
 
-#remove temporal folders
+echo "Cleaning up: remove temporal folders"
 cd -
 rm -rf $tmp_folder
 sudo rm -rf $tmp_rootfs_folder
+
+echo "Done."
+echo
+echo "Plug the SD on the target (i.MX6) and select the correct boot-mode thorugh the Dip switch."
+echo

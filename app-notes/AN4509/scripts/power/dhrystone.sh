9a2e618085c3534cac13a2819da1cc8fc2c15339
Leonardo Sandoval <leonardo.sandoval@freescale.com>
2013-01-28 11:17:24 -0600

    an4509: remove files due to past folder renaming

--- app-notes/AN4509/scripts/power/dhrystone.sh
+++
@@ -1,2 +0,0 @@
-#this creates a bunch of dhrystone processes, each lasting a few seconds, this will make the processor work at 100%.
-while true; do ./dry2 & done
--- app-notes/AN4509/scripts/power/power_measure1.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh -x
-
-while [ true ]
-do
-  ./dry2 &
-  ./dry2 &
-  ./dry2
-done

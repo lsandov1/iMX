fe23bf08a786a24a35089dd9f12febed9501421f
Leonardo Sandoval <leonardo.sandoval@freescale.com>
2013-01-28 11:27:31 -0600

    AN4509: less verbose output

--- app-notes/AN4509/flash_sd/mk_sd_an4509.sh
+++ app-notes/AN4509/flash_sd/mk_sd_an4509.sh
@@ -7,18 +7,19 @@ tmp_folder=`mktemp -d`
 cd $tmp_folder
 
 # Download scripts
-curl    https://raw.github.com/lsandoval/iMX/master/flash/mk_mx_sd > mk_mx_sd
-curl    https://raw.github.com/lsandoval/iMX/master/app-notes/AN4509/scripts/mk_image > mk_image
+curl    --silent https://raw.github.com/lsandoval/iMX/master/flash/mk_mx_sd > mk_mx_sd
+curl    --silent https://raw.github.com/lsandoval/iMX/master/app-notes/AN4509/scripts/mk_image > mk_image
 chmod +x ./mk_mx_sd ./mk_image
            
 # Download the binaries located on 
-wget    http://vfae-server:8000/AN/AN4509/rootfs.tar.bz2 \
+wget    --quiet \
+        http://vfae-server:8000/AN/AN4509/rootfs.tar.bz2 \
         http://vfae-server:8000/AN/AN4509/uImage \
         http://vfae-server:8000/AN/AN4509/uImageMaxPower
         
 # Untar the filesystem
 tmp_rootfs_folder=`mktemp -d`
-sudo tar xjvf rootfs.tar.bz2 -C $tmp_rootfs_folder
+sudo tar xjf rootfs.tar.bz2 -C $tmp_rootfs_folder
                 
 # Flash the SD
 sudo ./mk_image $dev \
